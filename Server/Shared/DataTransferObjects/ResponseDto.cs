31b512f9dff805fb321d741acb3d1fd65a7bc060
Muthuraj Subbian <muthurajsubbian@gmail.com>
2022-05-19 12:41:32 +0530

    ResponseDto

--- Server/Shared/DataTransferObjects/ResponseDto.cs
+++ Server/Shared/DataTransferObjects/ResponseDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SharedDto.DataTransferObjects.Masters
+namespace SharedDto.DataTransferObjects
 {
     public class ResponseDto
     {
